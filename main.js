52fa5a8d3b9dc8580cb35e8bba9325a3eb1fbddf
DigitalFoxy <90634529+DigitalFoxy@users.noreply.github.com>
2021-10-10 21:50:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log('Hello World!');
+
+function setButton () {
+ setTimeout(
+  function pp1() {
+   document.getElementById("p1").innerHTML = "Привет, Lera!";
+  }, 1000);
+  setTimeout(
+  function pp1() {
+   document.getElementById("p2").innerHTML = "Поздравляю тебя";
+  }, 1800);
+  setTimeout(
+   function pp1() {
+    document.getElementById("p3").innerHTML = "С Днём твоего рождения!";
+   }, 2600);
+   setTimeout(
+    function pp1() {
+     document.getElementById("p4").innerHTML = "Желаю счастья там и т.д...";
+    }, 3400);
+    setTimeout(
+     function pp1() {
+      document.getElementById("p5").innerHTML = "Ну и чтобы латинского на парах не было...";
+     }, 4500);
+     setTimeout(
+      function pp1() {
+       document.getElementById("p6").innerHTML = "И античной литры...";
+      }, 5500);
+      setTimeout(
+       function pp1() {
+        document.getElementById("p7").innerHTML = "Это такая дичь, лучше этого не узнавать...";
+       }, 6500);
+}
